80222f35b91fb8f585d59527a6e8e88e71412b16
Hannah Werman <hanna.w28@gmail.com>
2018-09-26 16:43:24 +0100

    optimized for pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,20 @@ module.exports = {
     },
     plugins: [
         {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: "GatsbyJS",
+                short_name: "GatsbyJS",
+                start_url: '/',
+                background_color: '#6b37bf',
+                theme_color: "6b37bf",
+                display: 'minimal-ui',
+                // icon: 'src/images/icon.png',
+            }
+        },
+        `gatsby-plugin-offline`,
+        `gatsby-plugin-react-helmet`,
+        {
             resolve: `gatsby-source-filesystem`,
             options: {
                 name: `src`,

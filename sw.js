e080c6d035b3beb9d8db444ed080ecb255113ba2
Hannah Werman <hanna.w28@gmail.com>
2019-02-15 11:54:45 -0600

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-563ffbe1b0b6cb586683.js"
+    "url": "webpack-runtime-c02eea57ca42de01a881.js"
   },
   {
     "url": "app-0ddca1ccb77d71f902c9.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d913aef4938b05e3e23334af7e25c3ae"
+    "revision": "8e983faf406c6e46ee429c387a581c47"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"

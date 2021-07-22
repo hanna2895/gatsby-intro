3b46089b14825a4a35d57237c848169374ab3ba7
Hannah Werman <hanna.w28@gmail.com>
2021-07-21 21:20:19 -0400

    updated skills, about, and resume links

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,43 +1,62 @@
-import React from "react"
-import { graphql, Link } from "gatsby"
-import Layout from "../components/layout"
-import './about/about.css';
-import hannaPhoto from './about/Hanna1.jpg';
-
+import React from "react";
+import { graphql, Link } from "gatsby";
+import Layout from "../components/layout";
+import "./about/about.css";
+import hannaPhoto from "./about/Hanna1.jpg";
 
 export default ({ data }) => (
   <Layout>
-    <div className='container'>
+    <div className="container">
       <div className="content">
         <div className="heading-holder">
-          <img src={hannaPhoto} className="home-img animated fadeInUp delay-1s" alt="Hanna standing in front of a door"/>
+          <img
+            src={hannaPhoto}
+            className="home-img animated fadeInUp delay-1s"
+            alt="Hanna standing in front of a door"
+          />
 
-            <h1 className="animated fadeInUp">Hi.</h1>
-            <h2 className="animated fadeInUp delay-1s">My name is Hannah Werman.</h2>
-            <h3 className="animated fadeInUp delay-2s">I’m a web developer and a travel & technology content writer.</h3>
+          <h1 className="animated fadeInUp">Hi.</h1>
+          <h2 className="animated fadeInUp delay-1s">
+            My name is Hannah Werman.
+          </h2>
+          <h3 className="animated fadeInUp delay-2s">
+            I’m a software engineer building towards the future of politics.
+          </h3>
 
-          <img src={hannaPhoto} className="home-img-mobile animated fadeInUp delay-2s" alt="Hanna standing in front of a door"/>
+          <p className="animated fadeInUp delay-3s">
+            I love building tech solutions that lower barriers to political
+            participation, because being ‘political’ shouldn’t be intimidating.
+            I’ve spent the last few years building products to make politics
+            more accessible, both through fighting fake news and misinformation,
+            and through making online political organizing and mobilization
+            simple for individuals and campaigns.
+          </p>
 
+          <p className="animated fadeInUp delay-3s">
+            I’ve worked with campaigns, startups, and entrepreneurs to create
+            beautiful, usable, and robust products that solve complex problems
+            in a practical way based on each organization’s unique needs.
+          </p>
 
-          <p className="animated fadeInUp delay-3s">I’m passionate about helping people leverage the power of technology to improve their day-to-day lives. <br /><br/> Whether that’s through cultivating an online presence for your small business, building an app to help you take your data on the road, automating your digital marketing processes, or writing technical content for your website or blog, I’m the right girl for the job.</p>
+          <p className="animated fadeInUp delay-3s">
+            Digital organizing and engagement is more important now than ever
+            before - let’s talk about how I can help YOU achieve your goals in
+            2020 and beyond.
+          </p>
 
-          <p className="animated fadeInUp delay-3s">I left my previous life in the Washington DC policy world in the spring of 2018 to pursue something more fulfilling. My background in analytical research, mathematics, fintech, and web development has given me the tools to take any complex problem or idea, create a workable solution, and present it to the community in an easily digestible way.</p>
-
-          <p className="animated fadeInUp delay-3s">I want to help YOU grow your business and automate your life. Let’s talk about how we can work together.</p>
-
-          <Link to={`/contact/`} className="btn btn-outline-dark animated fadeInUp delay-7s landing-link">Hire Me</Link>
-          <Link to={`/portfolio/`} className="btn btn-outline-dark animated fadeInUp delay-7s landing-link">View My Work </Link>
-
+          <Link
+            to={`/portfolio/`}
+            className="animated fadeInUp delay-5s landing-link"
+          >
+            View My Work{" "}
+          </Link>
         </div>
-
-
       </div>
 
       {/* <img src={hannaPhoto} className="animated fadeInUp delay-3s"/> */}
-
     </div>
   </Layout>
-)
+);
 
 // this is a page query.
 // page queries live outside the component definition and at the bottom of a file by convention
@@ -49,4 +68,4 @@ export const query = graphql`
       }
     }
   }
-`
+`;
--- src/pages/skills.js
+++ src/pages/skills.js
@@ -1,101 +1,127 @@
-import React from 'react';
-import Layout from '../components/layout';
-import { css } from "react-emotion"
-import Button from '@material-ui/core/Button';
+import React from "react";
+import Layout from "../components/layout";
+import { css } from "react-emotion";
+import Button from "@material-ui/core/Button";
 
-
-import './style.css';
+import "./style.css";
 
 export default () => (
-    <Layout>
-        <div class='container2 animated fadeInUp'>
-            <h1>Skills</h1>
-            <div class="skills-holder">
-                <div class="dev-skills">
-                <h4>Web Development</h4>
-                    <ul class="skill-list">
-                    <li>React</li>
-                    <li>Redux</li>
+  <Layout>
+    <div class="container2 animated fadeInUp">
+      <h1>Skills</h1>
+      <div class="skills-holder">
+        <div class="dev-skills">
+          <h4>Web Development</h4>
+          <ul class="skill-list">
+            <li>React</li>
+            <li>Redux</li>
+            <li>React Storybook</li>
 
-                    <li>HTML5</li>
-                    <li>CSS3</li>
+            <li>HTML5</li>
+            <li>CSS3</li>
 
-                    <li>JavaScript</li>
-                    <li>jQuery</li>
+            <li>JavaScript</li>
+            <li>Typescript</li>
+            <li>jQuery</li>
 
-                    <li>Node.js</li>
-                    <li>Express</li>
+            <li>Node.js</li>
+            <li>Express</li>
 
-                    <li>Python</li>
-                    <li>Django / Wagtail</li>
+            <li>Python</li>
+            <li>Django / Wagtail</li>
 
-                    <li>MongoDB</li>
-                    <li>Mongoose</li>
+            <li>MongoDB</li>
+            <li>Mongoose</li>
 
-                    <li>Bootstrap</li>
-                    <li>Less / Sass</li>
+            <li>Less / Sass</li>
 
-                    <li>PostgreSQL</li>
-                    <li>MySQL</li>
-                    <li>GraphQL</li>
+            <li>PostgreSQL</li>
+            <li>MySQL</li>
+            <li>GraphQL</li>
 
-                    <li>Ruby</li>
-                    <li>Ruby on Rails</li>
-                    <li>Sinatra</li>
+            <li>Ruby</li>
+            <li>Ruby on Rails</li>
+            <li>Sinatra</li>
 
-                    <li>Git</li>
-                    <li>GitHub</li>
-                    <li>Heroku</li>
+            <li>Git</li>
+            <li>GitHub</li>
+            <li>Heroku</li>
 
-                    <li>NGPVAN</li>
+            <li>NGPVAN</li>
 
-                    <li>Firebase</li>
-                    <li>Amazon Web Services</li>
-                    </ul>
-                </div>
+            <li>Firebase</li>
+            <li>Amazon Web Services</li>
+          </ul>
+        </div>
 
-                <div class="blog-skills">
+        <div class="blog-skills">
+          <h4>Blog, Social Media Management, and UX Design</h4>
+          <ul class="skill-list">
+            <li>Content Creation</li>
+            <li>WordPress</li>
+            <li>Squarespace</li>
+            <li>Adobe Photoshop</li>
+            <li>Adobe Lightroom</li>
+            <li>Adobe XD</li>
+            <li>Canva</li>
+            <li>Mailchimp</li>
+          </ul>
 
-                <h4>Blog, Social Media Management, and Graphic Design</h4>
-                    <ul class="skill-list">
-                    <li>Content Writing</li>
-                    <li>WordPress</li>
-                    <li>Squarespace</li>
-                    <li>Adobe Photoshop</li>
-                    <li>Adobe Lightroom</li>
-                    <li>Canva</li>
-                    <li>Later</li>
-                    <li>Crowdfire</li>
-                    <li>Twitter Advertising</li>
-                    <li>Mailchimp</li>
-                    </ul>
+          <h4 id="agile">Agile Management & Remote Work</h4>
+          <ul
+            class="skill-list"
+            className={css`
+              flex-wrap: nowrap;
+              justify-content: space-between;
+            `}
+          >
+            <li>Slack</li>
+            <li>Trello</li>
+            <li>Jira</li>
+            <li>Zoom</li>
+            <li>Google Hangouts</li>
+            <li>Certified Scrum Product Owner</li>
+          </ul>
+        </div>
+      </div>
 
-                    <h4 id="agile">Agile Management & Remote Work</h4>
-                    <ul class="skill-list" className={css`
-                        flex-wrap: nowrap;
-                        justify-content: space-between;
-                    `}>
-                        <li>Slack</li>
-                        <li>Trello</li>
-                        <li>Jira</li>
-                        <li>Zoom</li>
-                        <li>Google Hangouts</li>
-                    </ul>
-                </div>
-            </div>
-
-            <div class="links">
-
-                <h4 id="action">See more:</h4>
-                <div class="link-list">
-                    <a href="/content/HannahWermanApril2020Resume.pdf" target="_blank" rel="noopener noreferrer" className="skill-link">Resume</a>
-                    <a href="https://www.linkedin.com/in/hannah-werman/" target="_blank" rel="noopener noreferrer" className="skill-link">LinkedIn</a>
-                    <a href="https://github.com/hanna2895" target="_blank" rel="noopener noreferrer" className="skill-link">GitHub</a>
-                    <a href="https://twitter.com/hanna_codes" target="_blank" rel="noopener noreferrer" className="skill-link">Twitter</a>
-
-                </div>
-            </div>
-
-            </div>
-    </Layout>
-)
+      <div class="links">
+        <h4 id="action">See more:</h4>
+        <div class="link-list">
+          <a
+            href="https://drive.google.com/file/d/1BujbMw39o8R5tB3IJdTnqY1ewPMW5j02/view?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="skill-link"
+          >
+            Resume
+          </a>
+          <a
+            href="https://www.linkedin.com/in/hannah-werman/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="skill-link"
+          >
+            LinkedIn
+          </a>
+          <a
+            href="https://github.com/hanna2895"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="skill-link"
+          >
+            GitHub
+          </a>
+          <a
+            href="https://twitter.com/hanna_codes"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="skill-link"
+          >
+            Twitter
+          </a>
+        </div>
+      </div>
+    </div>
+  </Layout>
+);
